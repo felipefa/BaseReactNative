c56ab809a564cb5556e5c37b14fd392b6bcbbbca
Felipe Araujo <devaraujofelipe@gmail.com>
2019-10-16 11:18:40 -0300

    Create React Native app

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar backgroundColor="#333" barStyle="light-content" />
+      <SafeAreaView>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>Base React Native</Text>
+          <Text style={styles.sectionDescription}>
+            A base React Native app, configured with ESlint, Prettier and
+            Reactotron.
+          </Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    backgroundColor: '#333',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    padding: 30,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: '#ddd',
+  },
+});
+
+export default App;
